ba63882feba5b9bba77cb6056436d45d7efc4c23
Gabriel <bfrauches@gmail.com>
2019-01-06 04:30:15 -0200

    removendo node_modules

--- server/server.js
+++ server/server.js
@@ -1,1 +1,6 @@
 const express = require('express');
+const app = express();
+
+app.listen(3000, () => console.log(`Servidor rodando , porta: 3000`));
+
+app.get('/', (req, resp) => resp.send('/client/index.html'));
